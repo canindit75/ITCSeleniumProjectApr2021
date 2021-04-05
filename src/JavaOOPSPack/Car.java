0c82b98a9da99e43b4d74051eb14f98bb7ca4c6f
Anindita <anindita.chandra@gmail.com>
2021-04-05 15:42:04 +0530

    day1 -demo14 - Vehicle Car CarTest - Method Overdiding and Super Keyword

--- src/JavaOOPSPack/Car.java
+++ src/JavaOOPSPack/Car.java
@@ -1,5 +1,15 @@
 package JavaOOPSPack;
 //Demo of Inheritance
 public class Car extends Vehicle {
-
+	double speed = 50;
+	public Car(){
+		//System.out.println("Car - Default Constructor");
+		super();
+	}
+	public void Run(){
+		//System.out.println("Car - Run Method");
+		super.Run(); //This will point to the parent implementation
+		//please note we are making a call to parent implementation via child class method
+		System.out.println(super.speed); //100 
+	}
 }
--- src/JavaOOPSPack/Vehicle.java
+++ src/JavaOOPSPack/Vehicle.java
@@ -2,6 +2,9 @@ package JavaOOPSPack;
 //Demo of Inheritance
 public class Vehicle {
 	double speed = 100;
+	public Vehicle(){
+		System.out.println("Vehicle - Default Constructor");
+	}
 	public void Run(){
 		System.out.println("Vehicle - Run Method");
 	}
