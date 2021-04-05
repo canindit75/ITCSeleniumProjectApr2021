cd974ee7ef22820fbfb643e8414fbb2ac2ff92bb
Anindita <anindita.chandra@gmail.com>
2021-04-05 12:32:52 +0530

    day1 demo7 - switch statement

---
+++ src/Switch_Example.java
@@ -0,0 +1,27 @@
+public class Switch_Example {
+	public static void main(String[] args) {
+		int weekday = 5;
+		switch(weekday){
+			case 0 :System.out.println("Monday");break;
+			case 1 :System.out.println("Tuesday");break;
+			case 2 :System.out.println("Wednesday");break;
+			case 3 :System.out.println("Thursday");break;
+			case 4 :System.out.println("Friday");break;
+			case 5 :System.out.println("Saturday");break;
+			case 6 :System.out.println("Sunday");break;
+			default: System.out.println("Invalid Weekday");break;
+		}
+
+		String wday = "Mon";
+		switch(wday){
+			case "Mon" :System.out.println("Monday");break;
+			case "Tue" :System.out.println("Tuesday");break;
+			case "Wed" :System.out.println("Wednesday");break;
+			case "Thur":System.out.println("Thursday");break;
+			case "Fri" :System.out.println("Friday");break;
+			case "Sat" :System.out.println("Saturday");break;
+			case "Sun" :System.out.println("Sunday");break;
+			default: System.out.println("Invalid Weekday");break;
+		}
+	}
+}
