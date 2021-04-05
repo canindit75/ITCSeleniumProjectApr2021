2086288e7e76d1b86c693d16be97aca2b53944f2
Anindita <anindita.chandra@gmail.com>
2021-04-05 11:34:31 +0530

    day1 - 1st Version

---
+++ src/FirstJavaTest.java
@@ -0,0 +1,22 @@
+public class FirstJavaTest {
+	public static void main(String[] args) {
+		System.out.println("Selenium Training");
+		int qty = 200;
+		float price = 23.50f;
+		double TotalPrice ;
+		TotalPrice = price * qty;
+		char c = 'F';
+		boolean flag = true;
+		System.out.println("Value of qty = " +qty);
+		System.out.println("Value of price = " + price);
+		System.out.println("Total Price = " + TotalPrice);
+		System.out.println("Char value of c = "+ c);
+		System.out.println("Flag =  " +flag);		
+		int x = 100;
+		int y = 200;
+		String s = "Hello";
+		System.out.println(x+y+s);
+		System.out.println(s+x+y);
+		System.out.println(s+(x+y));		
+	}
+}
