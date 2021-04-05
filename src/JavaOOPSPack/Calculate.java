f5255bc48d77068ac295a14bc8034121ee163467
Anindita <anindita.chandra@gmail.com>
2021-04-05 14:22:10 +0530

    day1 - demo 9 Calculate - Example of Method

---
+++ src/JavaOOPSPack/Calculate.java
@@ -0,0 +1,33 @@
+package JavaOOPSPack;
+public class Calculate {
+	public void Sum1(int n1, int n2){
+		int sum = 0;
+	        sum = n1 + n2;
+	        System.out.println("sum = "  + sum);
+	}
+	public int Sum2(int n1, int n2){
+		int sum = 0;
+	        sum = n1 + n2;
+	       // System.out.println("sum = "  + sum);
+	       return sum; 
+	}
+	public static int Sum3(int n1, int n2){
+		int sum = 0;
+	        sum = n1 + n2;
+	       // System.out.println("sum = "  + sum);
+	       return sum; 
+	}
+	public static void main(String[] args){
+		 //to access an non static method of any class, first we need to create an instance of the class
+		   Calculate c1 = new Calculate();
+		   c1.Sum1(10,11);
+		   int s = c1.Sum2(23, 45);
+		   System.out.println("sum = " + s);
+		   s = c1.Sum2(10, 20);
+		   System.out.println("sum = " + s);
+		   s = c1.Sum2(30, 40);
+		   System.out.println("sum = " + s);
+		   s = Calculate.Sum3(45, 67); //static method allows to call the method directly without creating an object instance
+		   System.out.println("sum = " + s);
+		}
+}
--- src/JavaOOPSPack/Employee.java
+++ src/JavaOOPSPack/Employee.java
@@ -17,25 +17,5 @@ public class Employee{
   {
     System.out.println("empid = " + empid + " empname = " + empname + " comp = " + company);
   }
-  public static void main(String[] args){
-	  //create an instance or object of Employee class
-	  Employee emp1 = new Employee();
-	  emp1.insertEmpRecord(100, "Rakesh","ITC");
-	  emp1.displayEmpRecord();
-	  
-	  Employee emp2 = new Employee();
-	  emp2.empid = 101;
-	  emp2.empname = "Punitha";
-	  emp2.company = "Samsung";
-	  emp2.displayEmpRecord();
-	  
-	  Employee emp3 = new Employee();
-	  emp3.insertEmpRecord(102, "Lakshmi","Reliance");
-	  emp3.displayEmpRecord();
-	  
-	  Employee emp4 = new Employee();
-	  //when object is not initialized - it shows default values
-	  //for int it is 0 and for string it is nulls
-	  emp4.displayEmpRecord();
-  }
+
 }
