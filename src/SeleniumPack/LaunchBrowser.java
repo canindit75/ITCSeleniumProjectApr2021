e255884cd4b7d58fcbb7b4945e1027da497a47ac
Anindita <anindita.chandra@gmail.com>
2021-04-06 15:34:05 +0530

    launch browser - webdriver

--- src/SeleniumPack/LaunchBrowser.java
+++ src/SeleniumPack/LaunchBrowser.java
@@ -1,31 +1,30 @@
 package SeleniumPack;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 public class LaunchBrowser {
-	public static String browser = "ie";
+	public static WebDriver driver = null;
+	public static String browser = "chrome";
 	public static void main(String[] args) {
 		if(browser.equals("chrome")){
 			System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 			//to launch a browser - we need to create an instance of ChromeDriver class
 			//	Ctrl+ Shift+ O
-			ChromeDriver driver = new ChromeDriver();
-			//	navigate to the url 
-			driver.get("https://www.google.com/");
+			 driver = new ChromeDriver();
 		}
 		else if (browser.equals("firefox"))
 		{
 			System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
-			FirefoxDriver fdriver = new FirefoxDriver();
-			fdriver.get("https://www.google.com/");
+			driver = new FirefoxDriver();
 		}
 		else if(browser.equals("ie")){
 			System.setProperty("webdriver.ie.driver","./drivers/IEDriverServer.exe");
 			//2 more settings that needs to be performed 
 			//Zoom level must be at 100 %
 			//Protection Model Level of all security zones are either enabled or disabled
-			InternetExplorerDriver idriver = new InternetExplorerDriver();
-			idriver.get("https://www.google.com");
+			driver = new InternetExplorerDriver();
 		}
+		driver.get("https://www.google.com");
 	}
 }
