4e0ba294d181c23e9d22974756bcce301dc90ede
Anindita <anindita.chandra@gmail.com>
2021-04-06 17:24:48 +0530

    extract webtable

--- src/SeleniumPack/LocateElementByIdNameClass.java
+++ src/SeleniumPack/LocateElementByIdNameClass.java
@@ -1,4 +1,5 @@
 package SeleniumPack;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -22,8 +23,30 @@ public class LocateElementByIdNameClass {
 		 System.out.println("type value of ageTextbox = " +ageTextbox.getAttribute("type"));
 		 System.out.println("class value of ageTextbox = " +ageTextbox.getAttribute("class"));
 		 System.out.println("tagname of ageTextbox = " +ageTextbox.getTagName());
+		 //import java.util.List package
+		 List<WebElement> genderList = driver.findElements(By.name("csex"));
+		 //loop through the list of elements and the element which has value ="f"
+		 //id to identify the element because then the idea of using name locator strategy
+		 if(genderList.size()>0){
+			 for(WebElement gender : genderList){
+				 if(gender.getAttribute("value").equals("f")){
+					 if(!gender.isSelected()){
+						 gender.click();
+						 break;
+					 }
+				 }
+			 }
+		 }
+		 WebElement table = driver.findElement(By.className("cinfoT"));
+		 List<WebElement> rows =  table.findElements(By.tagName("tr"));
+		 for(WebElement row : rows){
+			 List<WebElement> cells = row.findElements(By.tagName("td"));
+			 for(WebElement cell : cells){
+				 System.out.print(cell.getText() + "\t");
+			 }
+		  System.out.print("\n");	 
+		 }
+ 
 		 
-
 	}
-
 }
