724ae367e55067006284cc378e5eef01d06a619e
Anindita <anindita.chandra@gmail.com>
2021-04-08 11:13:25 +0530

    caloriecalculator testng testcase

---
+++ src/testngPack/CalorieCalculatorTestCase.java
@@ -0,0 +1,57 @@
+package testngPack;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CalorieCalculatorTestCase {
+	WebDriver driver = null;
+	@Test
+	public void EnterCalorieDetails() throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		 driver = new ChromeDriver();
+		 driver.get("https://www.calculator.net/calorie-calculator.html");
+		 driver.manage().window().maximize();
+		 
+		// WebElement ageTextbox = driver.findElement(By.id("cage"));
+		 WebElement ageTextbox = driver.findElement(By.xpath("//*[@id='cage']"));
+		 ageTextbox.clear();
+		 ageTextbox.sendKeys("45");
+		 System.out.println("id value of ageTextbox = " +ageTextbox.getAttribute("id"));
+		 System.out.println("name value of ageTextbox = " +ageTextbox.getAttribute("name"));
+		 System.out.println("value of ageTextbox = " +ageTextbox.getAttribute("value"));
+		 System.out.println("type value of ageTextbox = " +ageTextbox.getAttribute("type"));
+		 System.out.println("class value of ageTextbox = " +ageTextbox.getAttribute("class"));
+		 System.out.println("tagname of ageTextbox = " +ageTextbox.getTagName());
+		 //import java.util.List package
+		 //List<WebElement> genderList = driver.findElements(By.name("csex"));
+		 List<WebElement> genderList = driver.findElements(By.xpath("//*[@name='csex']"));
+		 //loop through the list of elements and the element which has value ="f"
+		 //id to identify the element because then the idea of using name locator strategy
+		 if(genderList.size()>0){
+			 for(WebElement gender : genderList){
+				 if(gender.getAttribute("value").equals("f")){
+					 if(!gender.isSelected()){
+						 gender.click();
+						 break;
+					 }
+				 }
+			 }
+		 }
+		 WebElement table = driver.findElement(By.className("cinfoT"));
+		 List<WebElement> rows =  table.findElements(By.tagName("tr"));
+		 for(WebElement row : rows){
+			 List<WebElement> cells = row.findElements(By.tagName("td"));
+			 for(WebElement cell : cells){
+				 System.out.print(cell.getText() + "\t");
+			 }
+		  System.out.print("\n");	 
+		 }
+		 
+		 Thread.sleep(3000);
+		 driver.close();
+ 	}
+}
+
